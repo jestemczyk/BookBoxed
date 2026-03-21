983b56733a02882c883111bd2347aa6859f31b2e
Chtoś Niechtaŭski <mikhailalexv12@gmail.com>
2026-03-21 09:42:45 +0200

    fix main layout

--- src/pages/MainLayout.tsx
+++ src/pages/MainLayout.tsx
@@ -1,15 +1,3 @@
-import { useState } from "preact/hooks";
-import { Link, Outlet, useLocation } from "react-router";
-
 export const MainLayout = () => {
-  const location = useLocation();
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const navItems = [
-    { name: "BOOKS", path: "/books" },
-    { name: "SHELVES", path: "/book-shelves" },
-    { name: "SIGN IN", path: "/sign-in" },
-    { name: "CREATE ACCOUNT", path: "/registration" },
-  ];
-
   return <div>MainLayout</div>;
 };
