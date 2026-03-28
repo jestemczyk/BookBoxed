de645e309c41d26fa0f6a50f42c6b833127b321f
Chtoś Niechtaŭski <mikhailalexv12@gmail.com>
2026-03-28 10:28:11 +0200

    Feature/google books api (#8)
    * Add google books api
    
    * feat: add basicapi request structure
    
    * fix: fix imagesview
    
    * feat: add basic displaing structure

--- src/App.tsx
+++ src/App.tsx
@@ -9,19 +9,19 @@ import { SignIn } from "./pages/SignIn";
 import { Route, Routes } from "react-router";
 
 function App() {
-  return (
-    <Routes>
-      <Route element={<MainLayout />}>
-        <Route index element={<Home />} />
-        <Route path="/books" element={<Books />} />
-        <Route path="/book-shelves" element={<Bookshelves />} />
-        <Route path="/book-shelves/:shelfId" element={<Bookshelve />} />
-        <Route path="/book/:bookId" element={<Book />} />
-        <Route path="/registration" element={<Registration />} />
-        <Route path="/sign-in" element={<SignIn />} />
-      </Route>
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route element={<MainLayout />}>
+                <Route index element={<Home />} />
+                <Route path="/books" element={<Books />} />
+                <Route path="/book-shelves" element={<Bookshelves />} />
+                <Route path="/book-shelves/:shelfId" element={<Bookshelve />} />
+                <Route path="/book/:bookId" element={<Book />} />
+                <Route path="/registration" element={<Registration />} />
+                <Route path="/sign-in" element={<SignIn />} />
+            </Route>
+        </Routes>
+    );
 }
 
 export default App;
--- src/pages/Books.tsx
+++ src/pages/Books.tsx
@@ -1,11 +1,15 @@
+
 import { FiltersComponent } from "@/components/FiltersComponent";
 import { SearchComponent } from "@/components/SearchComponent";
+import { BooksComponent } from "@/components/BooksComponent";
 
 export const Books = () => {
   return (
     <div className="border-b border-gray-800 pb-4 mb-6">
       <SearchComponent />
       <FiltersComponent />
+      <BooksComponent />
     </div>
   );
+
 };
