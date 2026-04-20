cfc6e8a0d2970423ed0cd3059d90928ac9297da5
Chtoś Niechtaŭski <mikhailalexv12@gmail.com>
2026-04-21 00:43:08 +0300

    fix: fix skeleton adaptivity

--- src/components/BooksComponent.tsx
+++ src/components/BooksComponent.tsx
@@ -13,7 +13,7 @@ export const BooksComponent: React.FC = () => {
 
     if (isLoading) {
         return (
-            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 p-4">
+            <div className="flex flex-col items-center sm:grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 p-4">
                 {Array(10)
                     .fill(null)
                     .map((_, index) => (
