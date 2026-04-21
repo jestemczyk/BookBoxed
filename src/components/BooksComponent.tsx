d951c1ae0ec5870ef2bd9834c44d2cfeef6ccedc
Chtoś Niechtaŭski <mikhailalexv12@gmail.com>
2026-04-21 23:20:31 +0300

    Some work fixes for review

--- src/components/BooksComponent.tsx
+++ src/components/BooksComponent.tsx
@@ -14,7 +14,7 @@ export const BooksComponent: React.FC = () => {
     if (isLoading) {
         return (
             <div className="flex flex-col items-center sm:grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 p-4">
-                {Array(10)
+                {Array(30)
                     .fill(null)
                     .map((_, index) => (
                         <BookMiniCardSkeleton key={index} />
